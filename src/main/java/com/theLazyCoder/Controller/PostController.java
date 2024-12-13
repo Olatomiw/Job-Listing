6021be72a9ae3ec1ad326c46bc67c629f8241fe4
DevTom <72770851+Olatomiw@users.noreply.github.com>
2024-12-13 22:53:59 +0100

    Job posting done

--- src/main/java/com/theLazyCoder/Controller/PostController.java
+++ src/main/java/com/theLazyCoder/Controller/PostController.java
@@ -2,11 +2,10 @@ package com.theLazyCoder.Controller;
 
 import com.theLazyCoder.Model.Post;
 import com.theLazyCoder.PostRepository;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -21,14 +20,20 @@ public class PostController {
         this.postRepository = postRepository;
     }
 
+    @Operation(hidden = true)
     @RequestMapping(value = "/")
     public void redirect(HttpServletResponse response) throws IOException {
         response.sendRedirect("/swagger-ui.html");
     }
 
 
-    @GetMapping("post")
+    @GetMapping("posts")
     public List<Post> getAllPost(){
         return postRepository.findAll();
+    }
+
+    @PostMapping("/post")
+    public Post addPost(@RequestBody Post post){
+       return postRepository.save(post);
     }
 }
